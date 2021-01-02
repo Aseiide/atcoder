6d83693253224ffb900ee6675230b375d399872c
Aseiide <eiseiide@gmail.com>
2021-01-03 00:11:35 +0900

    回答を基に修正

--- abc187_a.rb
+++ abc187_a.rb
@@ -1,8 +1,12 @@
-a, b = gets.chomp.split.map(&:to_i)
+# 一旦stringでa,bを受け取る
+a, b = gets.chomp.split
 
-s = [a]
-t = [b]
+# 受け取ったa,bをintegerにして分割して配列に入れ、和を出す
+a_sum = a.chars.map(&:to_i).sum
+b_sum = b.chars.map(&:to_i).sum
 
-puts s.sum
-puts t.sum
-
+if a_sum >= b_sum
+  puts a_sum
+else
+  puts b_sum
+end

8b80ee06a9f14e0973b541052ddf25ec555c355d
Aseiide <eiseiide@gmail.com>
2021-01-10 22:08:03 +0900

    add 188

---
+++ abc188.rb
@@ -0,0 +1,14 @@
+N = gets.to_i
+A = gets.split.map(&:to_i)
+B = gets.split.map(&:to_i)
+# N回繰り返す
+# Aから一個ずつ、Bから一個ずつ、取り出す
+# 取り出したものをそれぞれ積をとる
+# それらの和をとる
+# 結果が0かどうか判定
+
+N.times do
+  A.each do |i|
+    ary =
+  end
+end

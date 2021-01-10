0a39f465beea3fd8bbec72bdc3f837562a140892
Aseiide <eiseiide@gmail.com>
2021-01-10 22:35:50 +0900

    matrix使ってみた　

--- abc188.rb
+++ abc188.rb
@@ -1,11 +1,9 @@
+require 'matrix'
 N = gets.to_i
 A = gets.split.map(&:to_i)
 B = gets.split.map(&:to_i)
 
-sum = 0
-N.times do
-  A.length.times { |i|sum += A[ i ] * B[ i ]}
-end
+sum = A.inner_product(B)
 if sum == 0
   puts "Yes"
 else

979eb2579b7a5cc72aff67ce2763e250dfeffd41
Aseiide <eiseiide@gmail.com>
2021-01-13 23:14:37 +0900

    add 188

--- abc188.rb
+++ abc188.rb
@@ -1,11 +1,12 @@
-require 'matrix'
 N = gets.to_i
-A = gets.split.map(&:to_i)
-B = gets.split.map(&:to_i)
-
-sum = A.inner_product(B)
-if sum == 0
+A = gets.split(" ").map(&:to_i)
+B = gets.split(" ").map(&:to_i)
+sum = 0
+ N.times do |i|
+   sum += A[i] * B[i]
+ end
+ if sum == 0
   puts "Yes"
-else
+ else
   puts "No"
-end
+ end

511ed6cc86c8eeab756c7a535ed9d07fdfdf27d2
Aseiide <eiseiide@gmail.com>
2021-02-07 00:23:28 +0900

    ABC191-B

--- abc191.rb
+++ abc191.rb
@@ -1,14 +1,8 @@
 n, x = gets.chomp.split.map(&:to_i)
 a = gets.chomp.split.map(&:to_i)
-newary = a.uniq #重複した数字を取り除いた配列
-s = a.select{|v| a.count(v) > 1}.uniq #[]かぶったものが出力
-
-if [x] == s
-  f = newary - [x]
-  puts f.join(' ')
-elsif [x] != s
-  z = newary - [x]
-  puts z.join(' ')
+ary = a.reject {|item| item == x}
+if 
+  puts ary.join(' ')
 else
-  puts
+  puts 
 end
